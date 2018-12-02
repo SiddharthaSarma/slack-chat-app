46ac81063d3af514334a4a9c3dab717bb79f1e8d
Siddharthasarma <sidduinevitable@gmail.com>
2018-12-02 18:30:21 +0530

    Added functionality to navigate to app route after login

--- src/index.js
+++ src/index.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import { App } from './components/App';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import firebase from './firebase';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css';
 import './styles.css';
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route path="/" exact component={App} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </Router>
-);
+class Root extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.history.push('/');
+      }
+    });
+  }
+  render() {
+    return (
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const RootWithAuth = withRouter(Root);
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<Root />, rootElement);
+ReactDOM.render(
+  <Router>
+    <RootWithAuth />
+  </Router>,
+  rootElement
+);
